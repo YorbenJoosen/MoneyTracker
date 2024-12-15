66ee2086d761d7941c008c647bc3faf0784b0373
WebmasterIngeniumUA <11708254+DrSqua@users.noreply.github.com>
2024-12-15 15:47:12 +0100

    Added ticket constructor

--- src/main/app/ticket/Ticket.java
+++ src/main/app/ticket/Ticket.java
@@ -2,25 +2,35 @@ package main.app.ticket;
 
 import main.app.person.Person;
 
-import java.util.Currency;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Ticket {
     private final String ticketName;
+    private final TicketType ticketType;
     private final Person owner;
-    private final List<Map.Entry<Person, Currency>> personPriceList;
+    private final List<Map.Entry<Person, Integer>> personPriceList;
 
-    public Ticket(Person owner, String ticketName, List<Map.Entry<Person, Currency>> personPriceList) {
+    public Ticket(Person owner, String ticketName, TicketType ticketType, List<Map.Entry<Person, Integer>> personPriceList) {
         this.owner = owner;
         this.ticketName = ticketName;
+        this.ticketType = ticketType;
         this.personPriceList = personPriceList;
     }
 
-    public static Ticket create_equal_all(Person owner, String ticketName, Currency totalPrice, TicketType ticketType);
-    public static Ticket create_equal_list(Person owner, String ticketName, Currency totalPrice, List<Person> persons, TicketType ticketType);
+    public static Ticket create_equal_list(Person owner, String ticketName, Integer totalPrice, List<Person> persons, TicketType ticketType) {
+        int pricePerPerson = totalPrice / persons.size();
 
-    public static Ticket create_individual_list(Person owner, String ticketName, List<java.util.Map.Entry<Person, Currency>> personAndPriceList, TicketType ticketType) {
-        return new Ticket(owner, ticketName, personAndPriceList);
+        ArrayList<Map.Entry<Person, Integer>> personPriceList = new ArrayList<Map.Entry<Person, Integer>>() {};
+        for (Person person : persons) {
+            personPriceList.add(Map.entry(person, pricePerPerson));
+        }
+
+        return new Ticket(owner, ticketName, ticketType, personPriceList);
+    }
+
+    public static Ticket create_individual_list(Person owner, String ticketName, List<java.util.Map.Entry<Person, Integer>> personAndPriceList, TicketType ticketType) {
+        return new Ticket(owner, ticketName, ticketType, personAndPriceList);
     }
 }
